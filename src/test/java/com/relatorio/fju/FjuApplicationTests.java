6fc5dcb64ab2aad73c1534bf4d99047fd0a5ae38
João Marcos <joao.oliveira@sensedia.com>
2021-01-20 18:47:31 -0300

    Commit inicial

---
+++ src/test/java/com/relatorio/fju/FjuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relatorio.fju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FjuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
